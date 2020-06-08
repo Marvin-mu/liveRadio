bf9c1606e8cd60262c228a5490b3ed21b37aea70
marvin <muchaowen@yeah.net>
2020-06-08 22:09:19 +0800

    commit

---
+++ videosurface.h
@@ -0,0 +1,13 @@
+#ifndef VIDEOSURFACE_H
+#define VIDEOSURFACE_H
+
+#include <QObject>
+
+class VideoSurface : public QAbstractVideoSurface
+{
+    Q_OBJECT
+public:
+    VideoSurface();
+};
+
+#endif // VIDEOSURFACE_H
